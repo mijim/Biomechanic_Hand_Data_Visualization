aae43454900f6a6c0d0aaea0248146c3a723b655
mijim <migueljimenezbenajes@gmail.com>
2018-02-09 10:20:55 +0100

    Update visualize_data.py for read VMG30 data and VMG30 data example

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 python visualize_data.py \<option\> \<file\>
 
+Supported options:
+\t vmg30 - for vmg30 data
+\t cybh - for cyberhand data
+
 Example usage: python visualize_data.py cybh data_example/cyberglobe/Ensayo_2_CGL_Cable.txt
+
+Example usage2: visualize_data.py vmg30 data_example/vmg30/20171103calib_miguel_1/Ensayo_15_VMR_Cable.txt
--- visualize_data.py
+++ visualize_data.py
@@ -4,6 +4,124 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+def plotVMG30(file):
+    f11,f12,f21,f22,f31,f32,f41,f42,f51,f52,abd1,abd2,abd3,abd4,pa,tco,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,hroll,hpitch,hyaw,wroll,wpitch,wyaw = [],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]
+    line_num = 0
+
+    for line in file:
+        if(line_num > 1):
+            line_array = line.split('\t')
+
+            f11.append(line_array[1])
+            f12.append(line_array[2])
+            f21.append(line_array[3])
+            f22.append(line_array[4])
+
+            f31.append(line_array[5])
+            f32.append(line_array[6])
+            f41.append(line_array[7])
+            f42.append(line_array[8])
+
+            f51.append(line_array[9])
+            f52.append(line_array[10])
+            pa.append(line_array[15])
+            tco.append(line_array[16])
+
+            abd1.append(line_array[11])
+            abd2.append(line_array[12])
+            abd3.append(line_array[13])
+            abd4.append(line_array[14])
+
+            p1.append(line_array[17])
+            p2.append(line_array[18])
+            p3.append(line_array[19])
+            p4.append(line_array[20])
+            p5.append(line_array[21])
+
+            p6.append(line_array[22])
+            p7.append(line_array[23])
+            p8.append(line_array[24])
+            p9.append(line_array[25])
+            p10.append(line_array[26])
+
+            hroll.append(line_array[27])
+            hpitch.append(line_array[28])
+            hyaw.append(line_array[29])
+
+            wroll.append(line_array[30])
+            wpitch.append(line_array[31])
+            wyaw.append(line_array[32])
+        line_num+=1
+
+    max_yticks = 3
+    yloc = plt.MaxNLocator(max_yticks)
+
+    fig_size = plt.rcParams["figure.figsize"]
+    fig_size[0] = 8
+    fig_size[1] = 7.7
+    plt.rcParams["figure.figsize"] = fig_size
+
+    f, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8) = plt.subplots(8, sharex=True, sharey=True)
+    f.canvas.set_window_title('Ensayo VMG30')
+    ax1.yaxis.set_major_locator(yloc)
+    ax2.yaxis.set_major_locator(yloc)
+    ax3.yaxis.set_major_locator(yloc)
+    ax4.yaxis.set_major_locator(yloc)
+    ax5.yaxis.set_major_locator(yloc)
+    ax6.yaxis.set_major_locator(yloc)
+    ax7.yaxis.set_major_locator(yloc)
+    ax8.yaxis.set_major_locator(yloc)
+
+    ax1.plot(f11, label="f11" ,linewidth=1)
+    ax1.plot(f12, label="f12" ,linewidth=1)
+    ax1.plot(f21, label="f21",linewidth=1)
+    ax1.plot(f22, label="f22",linewidth=1)
+    ax1.legend(loc=4)
+
+    ax2.plot(f31, label="f31",linewidth=1)
+    ax2.plot(f32, label="f32",linewidth=1)
+    ax2.plot(f41, label="f41",linewidth=1)
+    ax2.plot(f42, label="f42",linewidth=1)
+    ax2.legend(loc=4)
+
+    ax3.plot(f51, label="f51",linewidth=1)
+    ax3.plot(f52, label="f52",linewidth=1)
+    ax3.plot(pa, label="pa",linewidth=1)
+    ax3.plot(tco, label="tco",linewidth=1)
+    ax3.legend(loc=4)
+
+    ax4.plot(abd1, label="abd1",linewidth=1)
+    ax4.plot(abd2, label="abd2",linewidth=1)
+    ax4.plot(abd3, label="abd3",linewidth=1)
+    ax4.plot(abd4, label="abd4",linewidth=1)
+    ax4.legend(loc=4)
+
+    ax5.plot(p1, label="p1",linewidth=1)
+    ax5.plot(p2, label="p2",linewidth=1)
+    ax5.plot(p3, label="p3",linewidth=1)
+    ax5.plot(p4, label="p4",linewidth=1)
+    ax5.plot(p5, label="p5",linewidth=1)
+    ax5.legend(loc=4)
+
+    ax6.plot(p6, label="p6",linewidth=1)
+    ax6.plot(p7, label="p7",linewidth=1)
+    ax6.plot(p8, label="p8",linewidth=1)
+    ax6.plot(p9, label="p9",linewidth=1)
+    ax6.plot(p10, label="p10",linewidth=1)
+    ax6.legend(loc=4)
+
+    ax7.plot(hroll, label="hroll",linewidth=1)
+    ax7.plot(hpitch, label="hpitch",linewidth=1)
+    ax7.plot(hyaw, label="hyaw",linewidth=1)
+    ax7.legend(loc=4)
+
+    ax8.plot(wroll, label="wroll",linewidth=1)
+    ax8.plot(wpitch, label="wpitch",linewidth=1)
+    ax8.plot(wyaw, label="wyaw",linewidth=1)
+    ax8.legend(loc=4)
+
+    plt.subplots_adjust(bottom=0.05, left=0.09, top=0.97, hspace=0.00, right=0.96)
+    plt.show()
 
 def plotCyberH(file):
     roll1, mcf1, ifd1, a1, mcf2, ifp2, ifd2, a2, mcf3, ifp3, ifd3, a3, mcf4, ifp4, ifd4, a4, mcf5, ifp5, ifd5, a5, roll5, c, c_ano, usado = [],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]
@@ -47,7 +165,7 @@ def plotCyberH(file):
 
     fig_size = plt.rcParams["figure.figsize"]
     fig_size[0] = 6
-    fig_size[1] = 8
+    fig_size[1] = 7.7
     plt.rcParams["figure.figsize"] = fig_size
 
     f, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(6, sharex=True, sharey=True)
@@ -100,8 +218,13 @@ def plotCyberH(file):
 def main():
     op = sys.argv[1]
     file = open(sys.argv[2], "r")
+
     if(op=="cybh"):
         plotCyberH(file)
+    elif(op=="vmg30"):
+        plotVMG30(file)
+    else:
+        print("Unsuported operation: " + op)
 
 if __name__== "__main__":
     main()
